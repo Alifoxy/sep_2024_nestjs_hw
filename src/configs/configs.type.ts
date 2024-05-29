e1932db33ae3a1756ba77dc9e96a0fc7603a56e3
Alifoxy <nightfoxy666@gmail.com>
2024-05-29 19:57:24 +0300

    commit1

---
+++ src/configs/configs.type.ts
@@ -0,0 +1,32 @@
+export type Config = {
+    app: AppConfig;
+    database: DatabaseConfig;
+    redis: RedisConfig;
+    sentry: SentryConfig;
+};
+
+export type SentryConfig = {
+    dsn: string;
+    debug: boolean;
+    env: string;
+};
+
+
+export type AppConfig = {
+    port: number;
+    host: string;
+};
+
+export type DatabaseConfig = {
+    port: number;
+    host: string;
+    user: string;
+    password: string;
+    dbName: string;
+};
+
+export type RedisConfig = {
+    port: number;
+    host: string;
+    password: string;
+};
